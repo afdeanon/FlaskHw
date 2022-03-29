026a53bfbe43854a5223c21405d94fb7d0b05f0d
Faith Deanon <afdeanon@gmail.com>
2022-03-28 22:08:57 -0700

    question 1

---
+++ home.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template_string
+myapp_obj = Flask(__name__)
+@myapp_obj.route("/")
+def home():
+        name = "Lisa"
+        city_names = ["Paris", "London", "Rome", "Tahiti"]
+        return '''
+<html>
+<head>
+        <title>Flask Project</title>
+</head>
+<body>
+        <h1>Welcome ''' + name + '''!</h1>
+        <a href=“www.google.com”>not google</a>
+        <ul>
+                <li>''' + city_names[0] + '''</li>
+                <li>''' + city_names[1] + '''</li>
+                <li>''' + city_names[2] + '''</li>
+                <li>''' + city_names[3] + '''</li>
+        </ul>
+</body>
+</html>
+'''
+myapp_obj.run()
