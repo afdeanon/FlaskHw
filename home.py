768e7448ae75070f95f59f9f477af3436a26c050
Faith Deanon <afdeanon@gmail.com>
2022-03-29 18:42:03 -0700

    edit

--- home.py
+++ home.py
@@ -13,7 +13,7 @@ def home():
 </head>
 <body>
         <h1>Welcome ''' + name + '''!</h1>
-	<a href="https://www.google.com/">not google</a>
+	<a href="https://www.google.com/" target="_blank">not google</a>
         <ul>
                 <li>''' + city_names[0] + '''</li>
                 <li>''' + city_names[1] + '''</li>
@@ -23,3 +23,4 @@ def home():
 </body>
 </html>
 '''
+myapp_obj.run()
