ba9313d3eeae4a6afecba227d2966987eed3028e
Faith Deanon <afdeanon@gmail.com>
2022-03-31 16:40:04 -0700

    edit

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,8 @@
 from app import myapp_obj
-from flask import Flask, render_template, flash, render_template_string
+from flask import Flask, flash, render_template
+city_names = ["Paris", "London", "Rome", "Tahiti"]
 @myapp_obj.route("/")
-
 def home():
-        city_names = ["Paris", "London", "Rome", "Tahiti"]
-        return render_template('home.html', name = "Lisa", city_names = city_names)
+	cities = city_names
+	return render_template('home.html', name = "Lisa", city_names = cities)
 
-home()
